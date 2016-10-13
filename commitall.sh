0359dbbd577298f332d1d2b3faad9608771ad314
Chris Smith <chris@hichris.com>
2016-10-13 10:18:33 -0400

    Removed commitall.sh.

--- commitall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../aura      && git diff --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
-cd ../make-base && git diff --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
-cd ../mish      && git diff --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
-cd ../feta      && git diff --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
-
-echo ""
