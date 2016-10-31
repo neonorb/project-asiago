0ce5d5a06ceef1db77b807beed435ed3264d1866
Chris Smith <chris@hichris.com>
2016-10-31 09:47:06 -0400

    Commit script now shows diffs even after aborting commit.

--- commit.sh
+++ commit.sh
@@ -1,1 +1,1 @@
-git diff --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
+git diff HEAD --color=always | ( IFS=$'\n' read -r; if [ -n "$REPLY" ]; then ( printf '%s\n' "$REPLY"; cat ) | less --raw-control-chars; fi ) && git add -A && git commit
